a7712a990a8b3e73af89f64f44d1bd23a9a80628
philsmeby <phil.smeby@gmail.com>
2021-10-01 00:00:26 -0400

    basic character movement added

--- Source/CoopShooter/Private/SCharacter.cpp
+++ Source/CoopShooter/Private/SCharacter.cpp
@@ -73,4 +73,5 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAxis("Turn", this, &ASCharacter::AddControllerYawInput);
 	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ASCharacter::BeginCrouch);
 	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ASCharacter::EndCrouch);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 }
